d8bd7cd58a343ada5930438a4815841eb8483f88
Paul Sommer <paul@sommers.dev>
2026-01-12 08:27:02 +0100

    update ShoppingBasket

--- src/at/htlhl/sew2/oop/list/ShoppingBasket.java
+++ src/at/htlhl/sew2/oop/list/ShoppingBasket.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 
 class ShoppingBasket {
     void main() {
+        String input = IO.readln("Welche Zahl gibst du ein? ");
+        int number = Integer.parseInt(input);
+
         ArrayList<String> einkaufsListe = new ArrayList<>();
         while (true) {
             String s = IO.readln("Brauchen wir noch etwas j/n");
